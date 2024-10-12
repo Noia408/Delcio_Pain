fe11657b2fd63ae8e7f24c918b30d68334406b46
Noia408 <afonsoesteves203@gmail.com>
2024-10-12 16:23:09 +0100

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,37 @@
+'use client'
+
 import Image from "next/image";
+import LinkSocial from "./components/linksocial";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-full w-full flex max-sm:flex-col-reverse max-sm:justify-end gap-[2vw] relative">
+      <div className="w-[60%] h-full max-sm:w-full max-sm:h-fit flex flex-col justify-between gap-[1vw] max-sm:gap-[3vw]">
+        <div className="w-full h-[50%] max-sm:h-fit flex items-center max-sm:justify-start justify-center ">
+          <h1 className="text-white text-[9vw] max-sm:text-[13vw] underline decoration-[#CD7F32]">Delcio Pain</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="w-full h-fit border-l-4 border-[#CD7F32] flex flex-col gap-[1vw] max-sm:gap-[1.5vw] pl-[2vw] pb-[1vw]">
+          <h1 className="text-white text-[1.8vw] max-sm:text-[6vw]">Biografía</h1>
+          <span className="text-white text-[1.5vw] max-sm:text-[4vw]">Artista Independente que está navegando na vibe do Hip-Hop e Trap Soul cantando sempre sobre o seu cotidiano.</span>
+        </div>
+        <div className="w-full h-fit border-l-4 border-[#CD7F32] flex flex-col gap-[1vw] max-sm:gap-[1.5vw] pl-[2vw] pb-[1vw]">
+          <h1 className="text-white text-[1.8vw] max-sm:text-[6vw]">Minhas Mideas Sociais</h1>
+          <div className="grid-cols-3 max-sm:grid-cols-2 grid gap-[1vw] max-sm:gap-[3vw] w-full h-fit">
+            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial/></div>
+            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial/></div>
+            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial/></div>
+            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial/></div>
+            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial/></div>
+            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial/></div>
+            
+          </div>
+        </div>
+      </div>
+      <div className="w-[40%] h-full max-sm:w-full max-sm:h-[90vw] bg-[#CD7F32] relative">
+        <div className="w-full h-full absolute top-[1vw] right-[1vw] max-sm:top-[2vw] max-sm:right-[2vw] bg-white overflow-hidden flex justify-center">
+          <Image src={"/assets/Imagem WhatsApp 2024-10-07 às 20.36.54_1d8ebbfc.jpg"} className="h-full object-cover" alt="" width={700} height={1000} />
+        </div>
+      </div>
     </div>
   );
 }
