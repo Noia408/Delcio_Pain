9c783b34a7647e2ca5a41017c7093a5664e368fe
Noia408 <afonsoesteves203@gmail.com>
2024-10-13 15:01:00 +0100

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
           <div className="grid-cols-3 max-sm:grid-cols-2 grid gap-[1vw] max-sm:gap-[3vw] w-full h-fit">
             <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial nome="Spotify" logo="/assets/Spotify.png" links="https://open.spotify.com/artist/5erBqNwZ5aOgznp73drbmS?si=1hz5qnjXSIWLWNENwDmGoA" /></div>
             <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial nome="Sound Cloud" logo="/assets/SoundClound.png" links="https://on.soundcloud.com/MM8f5ArLineJ2EgN8" /></div>
-            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial nome="Facebook" logo="/assets/Facebook.png" links="https://www.facebook.com/profile.php?id=61550225032796" /></div>
+            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial nome="Facebook" logo="/assets/Facebook.png" links="https://www.facebook.com/61558611417071/posts/pfbid02raQZEchpMVUZD23SwkDdzQh737RRbYRFRnRet66kRMbGRJE7Hc2ZyeZvZoSHXFDl/?app=fbl" /></div>
             <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial nome="Youtube" logo="/assets/Youtube.png" links="https://youtube.com/@delciopain?si=SKr2-9tusVUTr74c" /></div>
-            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial nome="TikTok" logo="/assets/TikTok.png" links="/" /></div>
+            <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial nome="TikTok" logo="/assets/TikTok.png" links="https://www.tiktok.com/@delciopain?_r=1&_d=e7e39e5h8cfk75&sec_uid=MS4wLjABAAAAlb-JokE-mPVq6n4kqsBbGP7fmRr3TCQDS33qkQieadWaEDRWvRwGxIaUnbzxEjGw&share_author_id=7040509628606448646&sharer_language=pt&source=h5_m&u_code=dm64i2ckmje10f&ug_btm=b8727,b0&social_share_type=4&utm_source=copy&sec_user_id=MS4wLjABAAAAlb-JokE-mPVq6n4kqsBbGP7fmRr3TCQDS33qkQieadWaEDRWvRwGxIaUnbzxEjGw&tt_from=copy&utm_medium=ios&utm_campaign=client_share&enable_checksum=1&user_id=7040509628606448646&share_link_id=0656AE26-CBB5-48FC-80B3-F716D02939E0&share_app_id=1233" /></div>
             <div className="w-full h-[4vw] max-sm:h-[11vw] bg-[#cd7f32] relative"><LinkSocial nome="Instagram" logo="/assets/Instagram.png" links="https://www.instagram.com/delcio_pain44?igsh=NTc4MTIwNjQ2YQ==" /></div>
           </div>
         </div>
